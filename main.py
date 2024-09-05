f77559eac914e0e2f14430efceeca9f8122821db
axz369 <gp21a169@oecu.jp>
2024-09-05 11:35:12 +0900

    [delete] 唯一解生成のプログラム

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from modules.ConvertToNumber import ConvertToNumber
 from modules.Validation import Validation
 from modules.AddHintToLineSymmetry import AddHintToLineSymmetry
 from modules.UnifiedNumberOfHints import UnifiedNumberOfHints
-from modules.GenerateUniqueSolution import GenerateUniqueSolution
 from utility.displayBoards import displayBoards
 from utility.getUserChoice import getUserChoice
 from utility.generateSolutionBoard import generateSolutionBoard
@@ -78,31 +77,9 @@ def main():  # 数独パズルを生成, メインの関数
     print(f"\n選ばれた盤面: {selectedBoardName}")
 
     # 唯一解の生成
-    uniqueSolutionGenerator = GenerateUniqueSolution(selectedBoard, maxNumber)
-    uniqueSolution = uniqueSolutionGenerator.generateUniqueSolution()
 
-    # 唯一解が見つかったかどうかを確認
-    if uniqueSolution is not None:
-        print("\n唯一解が生成されました:")
-        uniqueSolutionGenerator.printBoard(uniqueSolution)
 
-        # 数字のまま保存
-        with open('numberOutput.json', 'w', encoding='utf-8') as f:
-            json.dump(uniqueSolution, f, ensure_ascii=False, indent=4)
-        print("numberOutput.jsonファイルが生成されました。")
-
-        # 数値から文字に変換
-        uniqueSolutionAsChars = converter.convertBack(uniqueSolution)
-
-        # JSONファイルとして保存
-        with open('output.json', 'w', encoding='utf-8') as f:
-            json.dump(uniqueSolutionAsChars, f, ensure_ascii=False, indent=4)
-
-        print("output.jsonファイルが生成されました。")
-    else:
-        print("\n唯一解が見つかりませんでした。")
-
-    return uniqueSolution is not None
+    
 
 
 if __name__ == "__main__":
